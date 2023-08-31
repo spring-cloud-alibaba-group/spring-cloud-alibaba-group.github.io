b92806abed0b569fa5cf2bf850fa24491a83e0fd
Ken Liu <ken.lj.hz@gmail.com>
2023-08-31 19:50:43 +0800

    Update custom-js.ts

--- src/custom-js.ts
+++ src/custom-js.ts
@@ -4,7 +4,7 @@ var _hmt = _hmt || [];
 (function () {
   if (ExecutionEnvironment.canUseDOM) {
     var hm = document.createElement('script');
-    hm.src = 'https://hm.baidu.com/hm.js?b04a83e339ead936fba7b14283194acc';
+    hm.src = 'https://hm.baidu.com/hm.js?8d45b13e5afb579f4083ee89048f70ac';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(hm, s);
   }
