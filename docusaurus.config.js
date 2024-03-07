be569ff2baa6230fcff1aa378a61ae16f6c028d4
cxhello <49056040+cxhello@users.noreply.github.com>
2024-03-07 16:28:21 +0800

    fix: editUrl 404 (#138)
    * fix: editUrl 404
    
    * fix: editUrl 404

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -107,8 +107,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: ({ docPath, locale }) => {
-            return `https://github.com/spring-cloud-alibaba-group/spring-cloud-alibaba-group.github.io/blob/main/i18n/${locale}/docusaurus-plugin-content-docs/current/${docPath}`;
+          editUrl: ({locale, version, docPath }) => {
+            version = version === "current" ? "current" : "version-" + version
+            return `https://github.com/spring-cloud-alibaba-group/spring-cloud-alibaba-group.github.io/blob/master/i18n/${locale}/docusaurus-plugin-content-docs/${version}/${docPath}`;
           },
           
           // last version 
@@ -134,7 +135,7 @@ const config = {
           blogSidebarTitle: "全部博文",
           blogSidebarCount: "ALL",
           editUrl: ({ blogPath, locale }) => {
-            return `https://github.com/spring-cloud-alibaba-group/spring-cloud-alibaba-group.github.io/blob/main/i18n/${locale}/docusaurus-plugin-content-blog/${blogPath}`;
+            return `https://github.com/spring-cloud-alibaba-group/spring-cloud-alibaba-group.github.io/blob/master/i18n/${locale}/docusaurus-plugin-content-blog/${blogPath}`;
           },
         },
         theme: {
