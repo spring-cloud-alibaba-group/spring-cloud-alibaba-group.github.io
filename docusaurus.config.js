c4f9baedfc5de9a1bcbdb17e19c342ca2f190e24
cxhello <49056040+cxhello@users.noreply.github.com>
2024-04-10 16:21:22 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,8 +42,8 @@ const config = {
     require.resolve("./src/custom-js.ts"),
   ],
   i18n: {
-    defaultLocale: "zh-cn",
-    locales: ["en-us", "zh-cn"],
+    defaultLocale: "default",
+    locales: ["en-us", "zh-cn", "default"],
     localeConfigs: {
       "en-us": {
         label: "En",
