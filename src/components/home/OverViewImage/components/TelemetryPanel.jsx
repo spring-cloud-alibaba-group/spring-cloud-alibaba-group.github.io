91a8093613d1405a8c4a060f855ef7aee1c28ccb
Hazel0928 <55099364+Hazel0928@users.noreply.github.com>
2024-05-16 10:08:10 +0800

    全景图 (#188)
    * update By Robot2024-05-15 22:30:21
    
    * fix: fix title
    
    * fix: 更改OT顺序
    
    ---------
    
    Co-authored-by: heimanba <371510756@qq.com>

--- src/components/home/OverViewImage/components/TelemetryPanel.jsx
+++ src/components/home/OverViewImage/components/TelemetryPanel.jsx
@@ -7,14 +7,11 @@ import { ProductPanel } from "./common/ProductPanel";
 export const TelemetryPanel = () => {
   return (
     <ProductPanel title="可观测">
-      {/* <Product
-        image={
-          "https://gw.alicdn.com/imgextra/i1/O1CN01JpJfq81KvYW9T8HlT_!!6000000001226-2-tps-216-174.png"
-        }
-        logo="https://gw.alicdn.com/imgextra/i3/O1CN01JPjgRH1fFyUa0oz1L_!!6000000003978-2-tps-72-72.png"
-        label="SkyWalking"
-        hoverContent={hoverSkyWalkingData}
-      /> */}
+      <Product
+        logo="https://img.alicdn.com/imgextra/i3/O1CN01jYzUO31nQywwUzAAv_!!6000000005085-2-tps-80-80.png"
+        label="iLogtail"
+        hoverContent={hoveriLogtailData}
+      />
       <Product
         logo="https://img.alicdn.com/imgextra/i3/O1CN01hTFdm51Jor72V1UQ9_!!6000000001076-2-tps-80-80.png"
         label="OpenTelemetry"
@@ -24,11 +21,6 @@ export const TelemetryPanel = () => {
         logo="https://img.alicdn.com/imgextra/i2/O1CN01xWWOPW1YLHmKw5I1Z_!!6000000003042-2-tps-80-80.png"
         label="Prometheus"
         hoverContent={hoverPrometheusData}
-      />
-      <Product
-        logo="https://img.alicdn.com/imgextra/i3/O1CN01jYzUO31nQywwUzAAv_!!6000000005085-2-tps-80-80.png"
-        label="iLogtail"
-        hoverContent={hoveriLogtailData}
       />
     </ProductPanel>
   );
