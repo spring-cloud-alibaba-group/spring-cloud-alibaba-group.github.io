ffef1238c517b49db379cd2db3358cdcc485643d
heimanba <371510756@qq.com>
2025-08-22 10:37:41 +0800

    update By Robot2025-08-22 10:37:41

--- src/constant.ts
+++ src/constant.ts
@@ -6,9 +6,9 @@ import { getEntries } from "astro:content";
 
 // 吊顶提示文案
 export const TOPBAR = {
-    "text": "下载《AI 应用 \u0026 AI Agent 开发新范式》电子书  了解构建 AI Agent 和 MCP Server 的一线实践。",
-    "mobileText": "下载《AI 应用 \u0026 AI Agent 开发新范式》电子书  了解构建 AI Agent 和 MCP Server 的一线实践。",
-    "link": "https://developer.aliyun.com/ebook/8442",
+    "text": "新一代 Agentic Coding 平台 Qoder 来了",
+    "mobileText": "新一代 Agentic Coding 平台 Qoder 来了",
+    "link": "https://qoder.com/",
     "target": "_blank",
     "display": false
 }
